041ec95ace339e5115334561de1cee02d9f41121
mdeeno <pshstyle93@gmail.com>
2026-02-19 17:49:08 +0900

    feat: 구글 서치콘솔 등록

---
+++ src/app/sitemap.js
@@ -0,0 +1,10 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'https://mdeeno.com',
+      lastModified: new Date(),
+      changeFrequency: 'daily',
+      priority: 1,
+    },
+  ];
+}
